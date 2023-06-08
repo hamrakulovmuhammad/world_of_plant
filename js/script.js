34301032407c135202585b0cf7a95525e56bfcb5
hamrakulovmuhammad <hamrakulovm010@gmail.com>
2023-06-09 03:38:40 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const anchers = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchers) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
